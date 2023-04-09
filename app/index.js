9e3823082eafc95a1c680a53e491f579e0dbe143
Raymond Ottun <sayjava@gmail.com>
2023-04-09 07:52:29 +0100

    build app with versions

--- app/index.js
+++ app/index.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const version = process.env.VERSION || 'v1'
-const behavior = (process.env.BEHAVIOR || 'good').toLowerCase()
 
 const good = (req, res) => {
 	res
@@ -36,7 +35,7 @@ const good = (req, res) => {
 		)
 }
 
-const bad = (_, res) => {
+const error = (_, res) => {
 	res
 		.status(500)
 		.setHeader('content-type', 'text/html')
@@ -56,11 +55,14 @@ const bad = (_, res) => {
 		)
 }
 
-const exit = (_, res) => {
+const quit = (_, res) => {
 	console.error('Server Exited')
 	process.exit(-1)
 }
 
+const slow = (_, res) => {
+	setTimeout(() => good(_, res), 3000)
+}
 
 app.get('/status', (_, res) => {
 	res
@@ -81,19 +83,21 @@ app.use(metrics({
 	requestDurationBuckets: [0.1, 0.5, 1, 1.5],
 	requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
 	responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-	customLabels: ['version', 'behavior'],
+	customLabels: ['version'],
 	transformLabels: (labels) => {
 		labels.version = version
-		labels.behavior = behavior
 	}
 }))
 
-switch (behavior) {
-	case 'bad':
-		app.get('/', bad)
+switch (version) {
+	case 'error':
+		app.get('/', error)
 		break;
-	case 'exit':
-		app.get('/', exit)
+	case 'quit':
+		app.get('/', quit)
+		break;
+	case 'slow':
+		app.get('/', slow)
 		break;
 	default:
 		app.get('/', good)
@@ -101,4 +105,4 @@ switch (behavior) {
 
 
 
-app.listen(PORT, () => console.log(`App listening at port ${PORT} with version ${version} and behavior ${behavior}`));
+app.listen(PORT, () => console.log(`App listening at port ${PORT} with version ${version}`));
