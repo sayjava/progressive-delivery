7aad5565319bab21d9fee5db145bcc9b5f952b36
Raymond Ottun <sayjava@gmail.com>
2023-04-09 07:25:35 +0100

    completed sample app

---
+++ app/index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const metrics = require('express-prometheus-middleware');
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const version = process.env.VERSION || 'v1'
+const behavior = (process.env.BEHAVIOR || 'good').toLowerCase()
+
+const good = (req, res) => {
+	res
+		.status(200)
+		.setHeader('content-type', 'text/html')
+		.send(
+			`<html>
+				<head>
+					<title>Progressive App ${version}</title>
+				</head>
+				<body style="background: grey; color: white">
+					<main>
+						<h1>
+							Welcome to progressive APP  <span style="color: blue">${version}</span>
+						</h2>
+						<div>
+							<ul>
+								<li>HOST: ${req.headers.host}</li>
+								<li>User Agent: ${req.headers["user-agent"]}</li>
+								<li>: ${req.headers["user-agent"]}</li>
+							</ul>
+						</div>
+					</main>
+				</body>
+			</html>`
+		)
+}
+
+const bad = (_, res) => {
+	res
+		.status(500)
+		.setHeader('content-type', 'text/html')
+		.send(
+			`
+			<html>
+				<head>
+					<title>Server Error</title>
+				</head>
+				<body>
+					<div>
+						Server Blew UP
+					</div>
+				</body>
+			</html>
+		`
+		)
+}
+
+const exit = (_, res) => {
+	console.error('Server Exited')
+	process.exit(-1)
+}
+
+
+app.get('/status', (_, res) => {
+	res
+		.setHeader('content-type', 'application/json')
+		.json({ ok: true })
+})
+
+
+app.use(expressWinston.logger({
+	transports: [
+		new winston.transports.Console()
+	]
+}))
+
+app.use(metrics({
+	metricsPath: '/metrics',
+	collectDefaultMetrics: false,
+	requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+	requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+	responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+	customLabels: ['version', 'behavior'],
+	transformLabels: (labels) => {
+		labels.version = version
+		labels.behavior = behavior
+	}
+}))
+
+switch (behavior) {
+	case 'bad':
+		app.get('/', bad)
+		break;
+	case 'exit':
+		app.get('/', exit)
+		break;
+	default:
+		app.get('/', good)
+}
+
+
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT} with version ${version} and behavior ${behavior}`));
