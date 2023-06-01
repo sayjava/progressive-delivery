fede06c46ae9423039c3f74e55937970fc80d312
Raymond Ottun <raymond.ottun@tesco.com>
2023-06-01 16:23:44 +0100

    zero downtime deployment

---
+++ TODO.md
@@ -0,0 +1,4 @@
+TODO
+[ ] - modify traffic script to log http code and header
+[ ] - create helm chart/kustomize chart to extract canary steps into inter-changeable documents
+[ ] - Better traffic generating scripts 
--- traffic.sh
+++ traffic.sh
@@ -1,13 +1,9 @@
 while true; do
     status_code=$(curl -s -o /dev/null -w "%{http_code}" http://prod.local:8080)
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://prod.local:8080)
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://prod.local:8080)
 
     if [[ $status_code -eq 200 ]]; then
         echo -e "\033[32m$status_code\033[0m"
     else
         echo -e "\033[31m$status_code\033[0m"
     fi
-
-    sleep 1
 done
